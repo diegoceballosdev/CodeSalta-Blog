395f09211b061ed2f86916b86e4454c80f7b81cc
Diego Ezequiel Ceballos <diegoceballosdev@gmail.com>
2026-02-20 15:39:54 -0300

    Cambios en config de next para ver img en produccion

--- blog-frontend/next.config.ts
+++ blog-frontend/next.config.ts
@@ -4,13 +4,27 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [
-      // Strapi: para local (desarrollo)
+      // Local
       {
         protocol: "http",
         hostname: "localhost",
         port: "1337",
       },
-      //al desplegar, debo colocalo tambien aqui:
+      // Strapi Cloud - API domain (por si acaso)
+      {
+        protocol: "https",
+        hostname: "legendary-crown-029891c091.strapiapp.com",
+      },
+      // 🔥 Strapi Cloud - MEDIA domain (este es el importante)
+      {
+        protocol: "https",
+        hostname: "legendary-crown-029891c091.media.strapiapp.com",
+      },
+      // 🔥 Strapi Cloud - MEDIA domain (Así no dependes del subdominio específico.):
+      {
+        protocol: "https",
+        hostname: "**.media.strapiapp.com",
+      },
     ],
     unoptimized: process.env.NODE_ENV === "development", // Esto es para que funcione con Strapi en local
   },
